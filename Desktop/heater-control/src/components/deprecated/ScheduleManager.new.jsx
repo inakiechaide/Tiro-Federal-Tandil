d09025745bd7d77f7b102fbda96643c9a7ed17c0
iñaki jose echaide <153095349+inakiechaide@users.noreply.github.com>
2025-12-14 09:12:35 -0300

    Delete Desktop/heater-control directory

--- Desktop/heater-control/src/components/deprecated/ScheduleManager.new.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ScheduleManager as ScheduleManagerComponent } from './schedule';
-
-const ScheduleManager = (props) => {
-  return <ScheduleManagerComponent {...props} />;
-};
-
-export default ScheduleManager;
